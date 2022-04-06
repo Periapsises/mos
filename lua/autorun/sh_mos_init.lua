68fc092420966801f319af87eed6362ce8f7f1b4
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-04-05 21:24:24 -0400

    Cleanup unecessary files

--- lua/autorun/sh_mos_init.lua
+++ lua/autorun/sh_mos_init.lua
@@ -2,7 +2,5 @@ Mos = {}
 
 AddCSLuaFile( "mos/editor/editor.lua" )
 AddCSLuaFile( "mos/editor/filebrowser.lua" )
-AddCSLuaFile( "mos/editor/mostextentry.lua" )
-AddCSLuaFile( "mos/language/parser.lua" )
 
 include( "mos/editor/editor.lua" )
