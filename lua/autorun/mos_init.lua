2c9346d73aad88a436bed9e88998029405c9ab5d
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-06-14 13:41:53 -0400

    Don't add non lua files as client files

--- lua/autorun/mos_init.lua
+++ lua/autorun/mos_init.lua
@@ -2,7 +2,7 @@ AddCSLuaFile()
 
 Mos = {}
 
--- Recursively adds clientside files from a specified folder
+-- Recursively adds serverside files from a specified folder only if the file is a lua file
 local function AddCSLuaFiles( path )
     local files, folders = file.Find( path .. "*", "LUA" )
 
@@ -11,7 +11,9 @@ local function AddCSLuaFiles( path )
     end
 
     for _, f in ipairs( files ) do
-        AddCSLuaFile( path .. f )
+        if f:sub( -4 ) == ".lua" then
+            AddCSLuaFile( path .. f )
+        end
     end
 end
 
