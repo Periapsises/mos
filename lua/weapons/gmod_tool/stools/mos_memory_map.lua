70c21b236a20cd1abc235540c255d5594606f576
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-08-15 23:05:24 -0400

    Add model selection to memory map

--- lua/weapons/gmod_tool/stools/mos_memory_map.lua
+++ lua/weapons/gmod_tool/stools/mos_memory_map.lua
@@ -12,6 +12,7 @@ if CLIENT then
     TOOL.Information = { "left" }
 end
 
+TOOL.ClientConVar.model = "models/mos/processor.mdl"
 TOOL.ClientConVar.addr_start = 0
 TOOL.ClientConVar.addr_end = 1024
 TOOL.ClientConVar.addr_map = 0
@@ -27,7 +28,7 @@ function TOOL:LeftClick( trace )
     ang.pitch = ang.pitch + 90
 
     local mapper = ents.Create( "mos_memory_map" )
-    mapper:SetModel( "models/mos/processor.mdl" )
+    mapper:SetModel( self:GetClientInfo( "model" ) )
     mapper:SetPos( trace.HitPos - trace.HitNormal * mapper:OBBMins().z )
     mapper:SetAngles( ang )
     mapper:SetOwner( self:GetOwner() )
@@ -43,9 +44,71 @@ function TOOL:LeftClick( trace )
     undo.Finish()
 end
 
+function TOOL:Think()
+    local model = self:GetClientInfo( "model" )
+    local ghost = self.GhostEntity or self:MakeGhostEntity( model, Vector(), Angle() )
+
+    if not IsValid( ghost ) then return end
+
+    if ghost:GetModel() ~= model then
+        ghost:SetModel( model )
+    end
+
+    local trace = self:GetOwner():GetEyeTrace()
+    local ent = trace.Entity
+
+    if IsValid( ent ) and ( ent:GetClass() == "mos_memory_map" or ent:IsPlayer() ) then
+        ghost:SetNoDraw( true )
+    else
+        local ang = trace.HitNormal:Angle()
+        ang.pitch = ang.pitch + 90
+
+        local min = ghost:OBBMins()
+
+        ghost:SetPos( trace.HitPos - trace.HitNormal * min.z )
+        ghost:SetAngles( ang )
+
+        ghost:SetNoDraw( false )
+    end
+end
+
 if SERVER then return end
 
 function TOOL.BuildCPanel( panel )
+    local availableModels = { ["models/mos/processor.mdl"] = true }
+
+    local wireModels = list.Get( "Wire_gate_Models" )
+    if next( wireModels ) then
+        table.Merge( availableModels, wireModels )
+    end
+
+    local starfallModels = list.Get( "Wire_gate_Models" )
+    if next( starfallModels ) then
+        table.Merge( availableModels, starfallModels )
+    end
+
+    local models = {}
+    for model in pairs( availableModels ) do
+        models[model] = { model = model }
+    end
+
+    panel:PropSelect( "Model", "mos_memory_map_model", models )
+
+    local presetContainer = vgui.Create( "DPanel" )
+    presetContainer:SetHeight( 24 )
+
+    local presetSave = vgui.Create( "DButton", presetContainer )
+    presetSave:SetText( "" )
+    presetSave:SetIcon( "icon16/disk.png" )
+    presetSave:SetWidth( 24 )
+    presetSave:Dock( RIGHT )
+
+    local presetSelection = vgui.Create( "DComboBox", presetContainer )
+    presetSelection:Dock( FILL )
+    presetSelection:AddChoice( "Default", nil, true )
+
+    panel:AddItem( presetContainer )
+
     local memStartSlider = vgui.Create( "DNumSlider" )
     memStartSlider:SetText( "Address Start" )
     memStartSlider:SetMinMax( 0, 65535 )
